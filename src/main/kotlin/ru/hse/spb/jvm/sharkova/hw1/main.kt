a62020f2664659f1cc63587598b08d98f3cf6182
sharkovadarya <sharkovadarya@gmail.com>
2018-09-24 10:42:47 +0300

    fixed formatting, lifted solve function to file level, deleted extra method calls

--- src/main/kotlin/ru/hse/spb/jvm/sharkova/hw1/main.kt
+++ src/main/kotlin/ru/hse/spb/jvm/sharkova/hw1/main.kt
@@ -5,56 +5,49 @@ import kotlin.Comparator
 import kotlin.collections.ArrayList
 import kotlin.math.max
 
-class Solver {
-    companion object {
-        fun solve(n: Int, w: Int, h: Int, g: IntArray, p: IntArray, t: IntArray): Array<Pair<Int, Int>> {
-            val answer = Array(n) { Pair(0, 0) }
+fun solve(n: Int, w: Int, h: Int, g: IntArray, p: IntArray, t: IntArray): Array<Pair<Int, Int>> {
+    val answer = Array(n) { Pair(0, 0) }
 
-            val maxT = t.max() ?: 0
-            val s = Array(max(w, h) + maxT) {ArrayList<Int>()}
-            for (i in 0 until n) {
-                s[p[i] - t[i] + maxT].add(i)
+    val maxT = t.max() ?: 0
+    val s = Array(max(w, h) + maxT) { ArrayList<Int>() }
+    for (i in 0 until n) {
+        s[p[i] - t[i] + maxT].add(i)
+    }
+
+    for (list in s.filter { it.isNotEmpty() }) {
+        val xs = ArrayList<Int>()
+        val ys = ArrayList<Int>()
+
+        for (i in list) {
+            if (g[i] == 1) {
+                xs.add(p[i])
+            } else {
+                ys.add(p[i])
+            }
+        }
+        xs.sort()
+        ys.sort()
+        list.sortWith(Comparator { u, v ->
+            if (u == null || v == null) {
+                return@Comparator 0
             }
 
-            val xs = ArrayList<Int>()
-            val ys = ArrayList<Int>()
-
-            for (list in s.filter { it -> it.isNotEmpty() }) {
-                for (i in list) {
-                    if (g[i] == 1) {
-                        xs.add(p[i])
-                    } else {
-                        ys.add(p[i])
-                    }
-                }
-                xs.sort()
-                ys.sort()
-                list.sortWith(Comparator{ u, v ->
-                    if (u == null || v == null) {
-                        return@Comparator 0
-                    }
-
-                    if (g[u] != g[v]) {
-                        if (g[u] == 2) -1 else 1
-                    } else {
-                        if (g[u] == 2) p[v].compareTo(p[u]) else p[u].compareTo(p[v])
-                    }
-                })
-
-                for (j in 0 until xs.size) {
-                    answer[list[j]] = Pair(xs[j], h)
-                }
-                for (j in 0 until ys.size) {
-                    answer[list[j + xs.size]] = Pair(w, ys[ys.size - j - 1])
-                }
-
-                xs.clear()
-                ys.clear()
+            if (g[u] != g[v]) {
+                if (g[u] == 2) -1 else 1
+            } else {
+                if (g[u] == 2) p[v].compareTo(p[u]) else p[u].compareTo(p[v])
             }
+        })
 
-            return answer
+        for (j in 0 until xs.size) {
+            answer[list[j]] = Pair(xs[j], h)
+        }
+        for (j in 0 until ys.size) {
+            answer[list[j + xs.size]] = Pair(w, ys[ys.size - j - 1])
         }
     }
+
+    return answer
 }
 
 fun main(args: Array<String>) {
@@ -74,7 +67,7 @@ fun main(args: Array<String>) {
         t[i] = reader.nextInt()
     }
 
-    val answer = Solver.solve(n, w, h, g, p, t)
+    val answer = solve(n, w, h, g, p, t)
 
     for (i in 0 until n) {
         println("${answer[i].first} ${answer[i].second}")
